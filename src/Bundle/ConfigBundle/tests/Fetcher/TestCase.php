1a2e86728ca7ae2222a37bba0b05c1231e524ed6
Filip Horvat <fico7489@gmail.com>
2024-03-12 11:10:08 +0100

    update

--- src/Bundle/ConfigBundle/tests/Fetcher/TestCase.php
+++ src/Bundle/ConfigBundle/tests/Fetcher/TestCase.php
@@ -2,8 +2,6 @@
 
 namespace FHPlatform\ConfigBundle\Tests\Fetcher;
 
-use FHPlatform\ConfigBundle\Fetcher\DTO\Connection;
-use FHPlatform\ConfigBundle\Fetcher\Global\ConnectionsFetcher;
 use FHPlatform\ConfigBundle\Tagged\TaggedProvider;
 use FHPlatform\ConfigBundle\Tests\Fetcher\Util\Es\Connection\ProviderConnection_Default;
 use FHPlatform\ConfigBundle\Tests\Fetcher\Util\Es\Connection\ProviderConnection_Default2;
@@ -38,31 +36,5 @@ class TestCase extends \FHPlatform\ConfigBundle\Tests\TestCase
         ];
 
         parent::setUp();
-    }
-
-    public function testFetchEntity(): void
-    {
-        /** @var ConnectionsFetcher $connectionsFetcher */
-        $connectionsFetcher = $this->container->get(ConnectionsFetcher::class);
-
-        $connections = $connectionsFetcher->fetch();
-
-        /** @var Connection $connection */
-        $connection = $connections[0];
-
-        /** @var Connection $connection2 */
-        $connection2 = $connections[1];
-
-        $this->assertEquals(2, count($connections));
-
-        $this->assertEquals('default', $connection->getName());
-        $this->assertEquals('prefix_default_', $connection->getPrefix());
-        $this->assertEquals(['test' => 'test'], $connection->getElasticaConfig());
-
-        $this->assertEquals('default2', $connection2->getName());
-        $this->assertEquals('prefix_default2_', $connection2->getPrefix());
-        $this->assertEquals(['test2' => 'test2'], $connection2->getElasticaConfig());
-
-        $this->assertEquals(1, 1);
     }
 }
