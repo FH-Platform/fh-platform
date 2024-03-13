155a7465fe7aea851e9e24950cf754aaf127dbd1
Filip Horvat <fico7489@gmail.com>
2024-03-13 15:57:28 +0100

    add

--- src/Bundle/ConfigBundle/tests/TestCase.php
+++ src/Bundle/ConfigBundle/tests/TestCase.php
@@ -3,16 +3,10 @@
 namespace FHPlatform\ConfigBundle\Tests;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\SchemaTool;
-use FHPlatform\ConfigBundle\Tagged\TaggedProvider;
 use FHPlatform\TestsBundle\Tests\Util\CommandHelper;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Filesystem\Filesystem;
 
-class TestCase extends KernelTestCase
+class TestCase extends \FHPlatform\TestsBundle\Tests\TestCase
 {
     private static array $events = [];
 
@@ -23,74 +17,5 @@ class TestCase extends KernelTestCase
     protected function setUp(): void
     {
         $this->prepareContainer();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        TaggedProvider::$includedClasses = [];
-    }
-
-    protected function prepareContainer(): void
-    {
-        // (1) boot the Symfony kernel
-        self::bootKernel();
-
-        // (2) use static::getContainer() to access the service container
-        $this->container = static::getContainer();
-
-        // (3) - CommandHelper
-        $this->commandHelper = new CommandHelper(self::$kernel);
-
-        // (4) - EntityManagerInterface
-        $this->entityManager = $this->container->get(EntityManagerInterface::class);
-
-        $this->migrateDb();
-    }
-
-    protected function eventsStartListen(string $eventClass): void
-    {
-        /** @var EventDispatcher $eventDispatcher */
-        $eventDispatcher = $this->container->get(EventDispatcherInterface::class);
-        $eventDispatcher->addListener($eventClass, function ($event) use ($eventClass): void {
-            self::$events[$eventClass][] = $event;
-        });
-    }
-
-    protected function eventsGet(string $eventClass): array
-    {
-        return self::$events[$eventClass] ?? [];
-    }
-
-    protected function eventsClear($eventClass): void
-    {
-        self::$events[$eventClass] = [];
-    }
-
-    private function migrateDb()
-    {
-        // updating a schema in sqlite database
-        $metaData = $this->entityManager->getMetadataFactory()->getAllMetadata();
-        $schemaTool = new SchemaTool($this->entityManager);
-        $schemaTool->updateSchema($metaData);
-    }
-
-    protected function save(array $entities): void
-    {
-        foreach ($entities as $entity) {
-            $this->entityManager->persist($entity);
-        }
-
-        $this->entityManager->flush();
-    }
-
-    protected function delete(array $entities): void
-    {
-        foreach ($entities as $entity) {
-            $this->entityManager->remove($entity);
-        }
-
-        $this->entityManager->flush();
     }
 }
--- src/Bundle/UtilBundle/tests/TestCase.php
+++ src/Bundle/UtilBundle/tests/TestCase.php
@@ -2,35 +2,10 @@
 
 namespace FHPlatform\UtilBundle\Tests;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\SchemaTool;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\Container;
-
-class TestCase extends KernelTestCase
+class TestCase extends \FHPlatform\TestsBundle\Tests\TestCase
 {
-    protected Container $container;
-    protected EntityManagerInterface $entityManager;
-
     protected function setUp(): void
     {
-        // (1) boot the Symfony kernel
-        self::bootKernel();
-
-        // (2) use static::getContainer() to access the service container
-        $this->container = static::getContainer();
-
-        // (3) - EntityManagerInterface
-        $this->entityManager = $this->container->get(EntityManagerInterface::class);
-
-        $this->migrateDb();
-    }
-
-    private function migrateDb()
-    {
-        // updating a schema in sqlite database
-        $metaData = $this->entityManager->getMetadataFactory()->getAllMetadata();
-        $schemaTool = new SchemaTool($this->entityManager);
-        $schemaTool->updateSchema($metaData);
+        $this->prepareContainer();
     }
 }
