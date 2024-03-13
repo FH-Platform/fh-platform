2136b3abba81872cebdf243e09071cd582a89078
Filip Horvat <fico7489@gmail.com>
2024-03-13 14:05:17 +0100

    1234

--- src/Bundle/DataSyncBundle/src/MessageHandler/DoctrineEntitiesChangedMessageHandler.php
+++ src/Bundle/DataSyncBundle/src/MessageHandler/DoctrineEntitiesChangedMessageHandler.php
@@ -7,6 +7,7 @@ use FHPlatform\ConfigBundle\DTO\Entity;
 use FHPlatform\ConfigBundle\Fetcher\DoctrineClassesNamesIndexFetcher;
 use FHPlatform\ConfigBundle\Fetcher\DoctrineClassesNamesRelatedFetcher;
 use FHPlatform\ConfigBundle\Fetcher\Entity\EntityFetcher;
+use FHPlatform\ConfigBundle\Fetcher\Entity\EntityRelatedFetcher;
 use FHPlatform\ConfigBundle\Fetcher\IndexFetcher;
 use FHPlatform\DataSyncBundle\Message\DoctrineEntitiesChangedMessage;
 use FHPlatform\PersistenceBundle\Event\ChangedEntityEvent;
@@ -17,14 +18,14 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 class DoctrineEntitiesChangedMessageHandler
 {
     public function __construct(
-        private readonly EntityHelper                       $entityHelper,
-        private readonly DataClient                         $dataClient,
-        private readonly EntityFetcher                      $entityFetcher,
-        private readonly IndexFetcher                       $indexFetcher,
-        private readonly DoctrineClassesNamesIndexFetcher   $doctrineClassesNamesIndexFetcher,
+        private readonly EntityHelper $entityHelper,
+        private readonly DataClient $dataClient,
+        private readonly EntityFetcher $entityFetcher,
+        private readonly IndexFetcher $indexFetcher,
+        private readonly EntityRelatedFetcher $entityRelatedFetcher,
+        private readonly DoctrineClassesNamesIndexFetcher $doctrineClassesNamesIndexFetcher,
         private readonly DoctrineClassesNamesRelatedFetcher $doctrineClassesNamesRelatedFetcher,
-    )
-    {
+    ) {
     }
 
     public function __invoke(DoctrineEntitiesChangedMessage $message): void
@@ -43,11 +44,14 @@ class DoctrineEntitiesChangedMessageHandler
             $type = $event->getType();
             $changedFields = $event->getChangedFields();  // TODO do upsert by ChangedFields
 
+            $entity = $this->entityHelper->refreshByClassNameId($className, $identifier);
+
             if (in_array($className, $classNamesIndex)) {
-                $this->prepareUpdates($className, $identifier, $type, $entitiesUpsert, $entitiesDelete);
+                $this->prepareUpdates($entity, $className, $identifier, $type, $entitiesUpsert, $entitiesDelete);
             }
 
-            if (in_array($className, $classNamesRelated)) {
+            if (in_array($className, $classNamesRelated) and $entity) {
+                $this->entityRelatedFetcher->fetch($entity);
                 // TODO
             }
         }
@@ -57,18 +61,19 @@ class DoctrineEntitiesChangedMessageHandler
         $this->dataClient->deleteBatch($entitiesDelete);
     }
 
-    private function prepareUpdates($className, $identifier, $type, &$entitiesUpsert, &$entitiesDelete): void
+    private function prepareUpdates(mixed $entity, string $className, mixed $identifier, string $type, array &$entitiesUpsert, array &$entitiesDelete): void
     {
         // TODO cache
         $indexes = $this->indexFetcher->fetchIndexesByClassName($className);
 
         foreach ($indexes as $index) {
-            $hash = $index->getConnection()->getName() . '_' . $index->getName() . '_' . $className . '_' . $identifier;
+            $hash = $index->getConnection()->getName().'_'.$index->getName().'_'.$className.'_'.$identifier;
+
+            // TODO return if hash exists
 
             if (ChangedEntityEvent::TYPE_DELETE === $type) {
                 $entitiesDelete[$hash] = new Entity($index, $identifier, [], false);
             } elseif (in_array($type, [ChangedEntityEvent::TYPE_UPDATE, ChangedEntityEvent::TYPE_CREATE])) {
-                $entity = $this->entityHelper->refreshByClassNameId($className, $identifier);
                 if (!$entity) {
                     $entitiesDelete[$hash] = new Entity($index, $identifier, [], false);
                 } else {
