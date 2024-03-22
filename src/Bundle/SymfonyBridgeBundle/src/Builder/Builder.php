aed49a48fb93b1194a7179ff631538accc8974bc
Filip Horvat <fico7489@gmail.com>
2024-03-22 15:20:57 +0100

    add

--- src/Bundle/SymfonyBridgeBundle/src/Builder/Builder.php
+++ src/Bundle/SymfonyBridgeBundle/src/Builder/Builder.php
@@ -6,6 +6,13 @@ use FHPlatform\Bundle\SymfonyBridgeBundle\EventDispatcher\EventDispatcher;
 use FHPlatform\Bundle\SymfonyBridgeBundle\MessageDispatcher\MessageDispatcher;
 use FHPlatform\Component\Client\Provider\ProviderInterface;
 use FHPlatform\Component\ClientElastica\ElasticaProvider;
+use FHPlatform\Component\Config\Config\Connection\ProviderConnection;
+use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorEntityInterface;
+use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorEntityRelatedInterface;
+use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorIndexInterface;
+use FHPlatform\Component\Config\Config\Provider\Interface\ProviderEntityInterface;
+use FHPlatform\Component\Config\Config\Provider\Interface\ProviderEntityRelatedInterface;
+use FHPlatform\Component\Config\Config\Provider\Interface\ProviderIndexInterface;
 use FHPlatform\Component\Persistence\Event\Event\ChangedEntitiesEvent;
 use FHPlatform\Component\Persistence\Event\EventDispatcher\EventDispatcherInterface;
 use FHPlatform\Component\Persistence\Event\EventHelper;
@@ -24,6 +31,8 @@ class Builder
         $this->buildProvider($container);
         $this->buildMessageDispatcher($container);
         $this->buildEventDispatcher($container);
+
+        $this->buildComponentConfig($container);
     }
 
     private function buildPersistence(ContainerBuilder $container): void
@@ -46,9 +55,9 @@ class Builder
     {
         // define message dispatcher (Symfony messenger, laravel queues, ...)
 
-        //TODO
+        // TODO
         $container->registerForAutoconfiguration(EntitiesChangedMessageHandler::class)->addTag('messenger.message_handler');
-        //$container->register(EventListener::class)->setAutowired(true);
+        // $container->register(EventListener::class)->setAutowired(true);
         $container->register(MessageDispatcher::class)->setAutowired(true);
         $container->addAliases([MessageDispatcherInterface::class => MessageDispatcher::class]);
     }
@@ -57,7 +66,7 @@ class Builder
     {
         // define event dispatcher (Symfony events, laravel events, ...)
 
-        //TODO
+        // TODO
         $container->registerForAutoconfiguration(EventListener::class)->addTag('kernel.event_listener', [
             'event' => ChangedEntitiesEvent::class,
             'method' => 'onChangedEntities',
@@ -65,5 +74,21 @@ class Builder
         $container->register(EventHelper::class)->setAutowired(true);
         $container->register(EventDispatcher::class)->setAutowired(true);
         $container->addAliases([EventDispatcherInterface::class => EventDispatcher::class]);
+    }
+
+    private function buildComponentConfig(ContainerBuilder $container): void
+    {
+        // connection
+        $container->registerForAutoconfiguration(ProviderConnection::class)->addTag('fh_platform.config.connection');
+
+        // provider
+        $container->registerForAutoconfiguration(ProviderIndexInterface::class)->addTag('fh_platform.config.provider.index');
+        $container->registerForAutoconfiguration(ProviderEntityInterface::class)->addTag('fh_platform.config.provider.entity');
+        $container->registerForAutoconfiguration(ProviderEntityRelatedInterface::class)->addTag('fh_platform.config.provider.entity_related');
+
+        // decorator
+        $container->registerForAutoconfiguration(DecoratorIndexInterface::class)->addTag('fh_platform.config.decorator.index');
+        $container->registerForAutoconfiguration(DecoratorEntityInterface::class)->addTag('fh_platform.config.decorator.entity');
+        $container->registerForAutoconfiguration(DecoratorEntityRelatedInterface::class)->addTag('fh_platform.config.decorator.entity_related');
     }
 }
--- src/Bundle/SymfonyBridgeBundle/src/SymfonyBridgeBundle.php
+++ src/Bundle/SymfonyBridgeBundle/src/SymfonyBridgeBundle.php
@@ -3,17 +3,6 @@
 namespace FHPlatform\Bundle\SymfonyBridgeBundle;
 
 use FHPlatform\Bundle\SymfonyBridgeBundle\Builder\Builder;
-use FHPlatform\Component\Config\Config\Connection\ProviderConnection;
-use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorEntityInterface;
-use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorEntityRelatedInterface;
-use FHPlatform\Component\Config\Config\Decorator\Interface\DecoratorIndexInterface;
-use FHPlatform\Component\Config\Config\Provider\Interface\ProviderEntityInterface;
-use FHPlatform\Component\Config\Config\Provider\Interface\ProviderEntityRelatedInterface;
-use FHPlatform\Component\Config\Config\Provider\Interface\ProviderIndexInterface;
-use FHPlatform\Component\Persistence\Event\Event\ChangedEntitiesEvent;
-use FHPlatform\Component\Persistence\Event\EventListener\EventListener;
-use FHPlatform\Component\Persistence\Message\MessageHandler\EntitiesChangedMessageHandler;
-use FHPlatform\Component\PersistenceDoctrine\Listener\DoctrineListener;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -21,26 +10,6 @@ class SymfonyBridgeBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        // connection
-        $container->registerForAutoconfiguration(ProviderConnection::class)->addTag('fh_platform.config.connection');
-
-        // provider
-        $container->registerForAutoconfiguration(ProviderIndexInterface::class)->addTag('fh_platform.config.provider.index');
-        $container->registerForAutoconfiguration(ProviderEntityInterface::class)->addTag('fh_platform.config.provider.entity');
-        $container->registerForAutoconfiguration(ProviderEntityRelatedInterface::class)->addTag('fh_platform.config.provider.entity_related');
-
-        // decorator
-        $container->registerForAutoconfiguration(DecoratorIndexInterface::class)->addTag('fh_platform.config.decorator.index');
-        $container->registerForAutoconfiguration(DecoratorEntityInterface::class)->addTag('fh_platform.config.decorator.entity');
-        $container->registerForAutoconfiguration(DecoratorEntityRelatedInterface::class)->addTag('fh_platform.config.decorator.entity_related');
-
-        $container->registerForAutoconfiguration(DoctrineListener::class)->setAutoconfigured(true)->setPublic(true)->setAutowired(true);
-        /*$container->registerForAutoconfiguration(DoctrineListener::class)->addTag(AsDoctrineListener::class, ['event' => Events::postPersist]);
-        $container->registerForAutoconfiguration(DoctrineListener::class)->addTag('doctrine.orm.entity_listener', ['event' => Events::postUpdate]);
-        $container->registerForAutoconfiguration(DoctrineListener::class)->addTag('doctrine.orm.entity_listener', ['event' => Events::postRemove]);
-        $container->registerForAutoconfiguration(DoctrineListener::class)->addTag('doctrine.orm.entity_listener', ['event' => Events::preRemove]);
-        $container->registerForAutoconfiguration(DoctrineListener::class)->addTag('doctrine.orm.entity_listener', ['event' => Events::postFlush]);*/
-
         (new Builder())->build($container);
     }
 }
