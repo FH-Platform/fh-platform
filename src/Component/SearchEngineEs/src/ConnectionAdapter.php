3e175457deb37fa99b3d76b279c1a2ad2f304953
Filip Horvat <fico7489@gmail.com>
2024-03-24 10:14:53 +0100

    update

---
+++ src/Component/SearchEngineEs/src/ConnectionAdapter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FHPlatform\Component\SearchEngineEs;
+
+use FHPlatform\Component\SearchEngine\Connection\ConnectionAdapterInterface;
+
+class ConnectionAdapter implements ConnectionAdapterInterface
+{
+}
--- tests.sh
+++ tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 packages=(
-  "Component//SearchEngineEsGuzzle"
-  "Component//SearchEngineEsElastica"
+  "Component/SearchEngineEsGuzzle"
+  "Component/SearchEngineEsElastica"
   "Component/Persistence"
   "Component/PersistenceDoctrine"
   "Bundle/SymfonyBridgeBundle"
