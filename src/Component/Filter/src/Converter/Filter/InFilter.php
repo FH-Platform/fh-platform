2b0cf35a546ec88fefbae506dd4be01bc59ffd5d
Filip Horvat <fico7489@gmail.com>
2024-03-27 14:39:08 +0100

    add

--- src/Component/Filter/src/Converter/Filter/InFilter.php
+++ src/Component/Filter/src/Converter/Filter/InFilter.php
@@ -4,6 +4,7 @@ namespace FHPlatform\Component\Filter\Converter\Filter;
 
 use Elastica\Query\AbstractQuery;
 use Elastica\Query\BoolQuery;
+use Elastica\Query\Exists;
 use Elastica\Query\Terms;
 use FHPlatform\Component\Filter\Converter\FilterInterface;
 
@@ -16,6 +17,23 @@ class InFilter implements FilterInterface
 
     public function convert(BoolQuery $query, string $field, mixed $value): AbstractQuery
     {
+        /*
+         $boolQuery = new BoolQuery();
+
+        if ($value !== null){
+            $termsQuery = new Terms($field, $value);
+            $query->addShould($termsQuery);
+        }else{
+            $existsQuery = new Exists($field);
+            $query->addShould($existsQuery);
+        }
+
+        $boolQuery->addMust($query);
+
+        return $boolQuery;
+
+         */
+
         $termsQuery = new Terms($field, $value);
         $query->addMust($termsQuery);
 
