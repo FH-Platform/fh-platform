0c6f809e916f0dec73411f9a5e348824b1e2d03d
Filip Horvat <fico7489@gmail.com>
2024-03-29 08:01:49 +0100

    add

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,4 @@
 
 return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
