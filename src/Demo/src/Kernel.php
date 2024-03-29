2b6a1ce5d913f5905616c7af7c0e13fb955d02c7
Filip Horvat <fico7489@gmail.com>
2024-03-29 08:04:33 +0100

    adda

--- src/Demo/src/Kernel.php
+++ src/Demo/src/Kernel.php
@@ -8,4 +8,19 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return '../../../var/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return '../../../var/logs';
+    }
+
+    /*public function getProjectDir(): string
+    {
+        return __DIR__.'/../';
+    }*/
 }
