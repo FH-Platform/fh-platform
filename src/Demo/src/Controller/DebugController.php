e200e8ab7c9d7359b8157ecbc5a388bc9d0e9445
Filip Horvat <fico7489@gmail.com>
2024-03-29 08:49:46 +0100

    adda

--- src/Demo/src/Controller/DebugController.php
+++ src/Demo/src/Controller/DebugController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DebugController extends  AbstractController
+class DebugController extends AbstractController
 {
     public function __construct(
         private readonly ConnectionsBuilder $connectionsBuilder,
@@ -19,8 +19,45 @@ class DebugController extends  AbstractController
     #[Route('/_fhplatform/debug/connections')]
     public function debugConnections(): JsonResponse
     {
+        $connections = (array)$this->connectionsBuilder->build();
+
+        $arr = json_decode(json_encode($connections), true);
+
+
+        $connectionsArray = [];
+
+        foreach ($connections as $connection) {
+            $connectionArray = $this->object_to_array($connection);
+
+            $indexesArray = [];
+            $indexes = $connection->getIndexes();
+            foreach ($indexes as $index) {
+                $indexArray = $this->object_to_array($index);
+
+                unset($indexArray['connection']);
+
+                $indexesArray[] = $indexArray;
+            }
+
+            $connectionArray['indexes'] = $indexesArray;
+
+            $connectionsArray[] = $connectionArray;
+        }
+
         return new JsonResponse(
-            $this->connectionsBuilder->build()
+            $connectionsArray
         );
+    }
+
+    private function object_to_array($object)
+    {
+        $reflectionClass = new \ReflectionClass(get_class($object));
+        $array = array();
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            $array[$property->getName()] = $property->getValue($object);
+            $property->setAccessible(false);
+        }
+        return $array;
     }
 }
