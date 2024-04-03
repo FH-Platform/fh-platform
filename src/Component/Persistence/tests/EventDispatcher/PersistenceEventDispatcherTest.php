78853dd56b819c43f9acf039d58d8292a24d9a6f
Filip Horvat <fico7489@gmail.com>
2024-04-03 09:25:55 +0200

    fixx

--- src/Component/Persistence/tests/EventDispatcher/PersistenceEventDispatcherTest.php
+++ src/Component/Persistence/tests/EventDispatcher/PersistenceEventDispatcherTest.php
@@ -53,11 +53,11 @@ class PersistenceEventDispatcherTest extends TestCase
         $this->assertEquals([], $event->getChangedFields());
 
         // pre delete
-        $this->eventsStartListen(ChangedEntityPreDelete::class);
-        $this->eventsClear(ChangedEntityPreDelete::class);
+        $this->eventsStartListen(ChangedEntity::class);
+        $this->eventsClear(ChangedEntity::class);
         $eventManager->dispatchPreDeleteEntity(Role::class, 1);
-        /** @var ChangedEntityPreDelete $event */
-        $event = $this->eventsGet(ChangedEntityPreDelete::class)[0];
+        /** @var ChangedEntity $event */
+        $event = $this->eventsGet(ChangedEntity::class)[0];
 
         $this->assertEquals(Role::class, $event->getClassName());
         $this->assertEquals(1, $event->getIdentifierValue());
