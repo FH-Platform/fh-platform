15876095d30610451b056952a1fcff961c636e97
Filip Horvat <fico7489@gmail.com>
2024-04-03 10:12:51 +0200

    1234

---
+++ src/Component/EventManager/tests/Util/FHPlatform/RoleProviderEntity.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FHPlatform\Component\EventManager\Tests\Util\FHPlatform;
+
+use FHPlatform\Component\Config\Config\Provider\ProviderEntity;
+use FHPlatform\Component\Config\DTO\Index;
+use FHPlatform\Component\DoctrineToEs\Tests\Util\Entity\Role\Role;
+use FHPlatform\Component\DoctrineToEs\Tests\Util\Entity\User;
+
+class RoleProviderEntity extends ProviderEntity
+{
+    public function getClassName(): string
+    {
+        return Role::class;
+    }
+
+    public function getConfigAdditional(Index $index, array $config): array
+    {
+        $config['doctrine_to_es'] =
+            [
+                'testString',
+                'users' => [
+                    'testString',
+                ]
+            ];
+
+        return $config;
+    }
+}
